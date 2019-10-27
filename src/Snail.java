afcbc1de3efbf79ca5cce48ff35ae5760c8f849e
hawkchiki <vakionika@gmail.com>
2019-10-27 10:57:27 +0300

    Пере-решение

--- src/Snail.java
+++ src/Snail.java
@@ -4,21 +4,21 @@ public class Snail {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Улитка за день забирается на");
-        int m=scanner.nextInt();
+        int m = scanner.nextInt();
         System.out.println("Правда за ночь она опускается на");
         int n = scanner.nextInt();
         System.out.println("За какое время она поднимется на");
         int s = scanner.nextInt();
         int c = 0;
         int i = 0;
-        int k = (m - n);
 
-
-         for (; i < s; i = i + k) {
-
-            c++;
+        while (true) {
+            i++;
+            c += m;
+            if (c >= m) break;
+            c -= n;
 
         }
-        System.out.println("За " + c + "дня");
+        System.out.println("За " + i + " дня");
     }
 }
