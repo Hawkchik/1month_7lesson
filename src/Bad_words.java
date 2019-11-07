26aaa44640caa609925a2bc1de3ab7a169b57bf2
ili <ili@zagruzka.com>
2019-11-07 15:39:58 +0300

    Debug for chat-bot

--- src/Bad_words.java
+++ src/Bad_words.java
@@ -2,16 +2,17 @@ public class Bad_words {
      String[] ploxo = {"Блядь", "fuck", "пиздец", "атата", " "};
 
     public boolean  check_bad_words(String c) {
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
 
             if (ploxo[i].toUpperCase().equals(c.toUpperCase())) {
 
+                return true;
             }
 
         }
 
 
-     return true;
+     return false;
 }
 
 
